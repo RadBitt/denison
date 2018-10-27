4f0466f35c995999ad7db48729ba6f41612df26e
Leo <leo@radbitt.com>
2018-10-27 09:03:03 -0700

    10-27-2018

---
+++ src/base,js.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAEeMUbSbK0yJs_6pnerHpa0YvWUQr7IOA",
+    authDomain: "denison-yacht-sales.firebaseapp.com",
+    databaseURL: "https://denison-yacht-sales.firebaseio.com",
+ });
+
+export default base;
