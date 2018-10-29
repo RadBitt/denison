30c79e3cf6f392d0a8ffbe9821c39567cc482915
Leo <leo@radbitt.com>
2018-10-29 07:17:55 -0700

    10-29-2018 React Router

---
+++ src/components/Router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {BrowserRouter , Route, Switch} from 'react-router-dom';
+import Dashboard from './Dashboard';
+
+const Router = () => (
+	<BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={Dashboard} />
+		</Switch>
+	</BrowserRouter>
+)
+
+export default Router;
