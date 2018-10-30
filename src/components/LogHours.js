bb2b1fe34916c218f4e4939dc72d4c47f2e63873
Leo <leo@radbitt.com>
2018-10-30 08:13:18 -0700

    10-30-2018 Draft Entry

--- src/components/LogHours.js
+++ src/components/LogHours.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import LogEntry from './child-components/LogEntry';
 
 class LogHours extends React.Component {
 	render() {
 		return(
-			<div>LogHours.js</div>
+			<div><LogEntry /></div>
 		);
 	}
 }
--- src/components/child-components/LogEntry.js
+++ src/components/child-components/LogEntry.js
@@ -3,11 +3,30 @@ import React from 'react';
 class LogEntry extends React.Component {
 
 	render() {
-		return(
-			<div>
-				
-			</div>
-		);
+	const logEntryObj = {
+	'Date': '10-30-2018',
+	'Job': {
+		'JobTitle': 'D520',
+		'JobHours': '10',
+		'JobDescription': [
+		'Engine Serive', 
+		'Bow Thruster Repair'
+			]
+		}
+	}
+
+	return(
+		<div>
+			<h2>{logEntryObj.Date}</h2>
+			<ul>
+				<li><h3>{logEntryObj.Job.JobTitle} : {logEntryObj.Job.JobHours} Hours</h3></li>
+				<ul> 
+					<li>{logEntryObj.Job.JobDescription[0]}</li>
+					<li>{logEntryObj.Job.JobDescription[1]}</li>
+				</ul>
+			</ul>
+		</div>
+	);
 	}
 }
 
