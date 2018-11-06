c68516e30e899023aa152904ca342cc5bfcd099f
Leo <leo@radbitt.com>
2018-11-06 07:44:58 -0800

    harvest api

--- public/server.js
+++ public/server.js
@@ -5,10 +5,23 @@ var path = require('path');
 var parser = require('body-parser'); 
 // var mailRouter = require('./mailer');
 // var apiRouter = require('./api');
+var https = require("https");
 
 var app = express();
 var port = 8080;
 var publicPath = path.resolve(__dirname, '../html');
+
+var options = {
+  protocol: "https:",
+  hostname: "api.harvestapp.com",
+  path: "/v2/users/me",
+  headers: {
+    "User-Agent": "Node.js Harvest API Sample",
+    "Authorization": "Bearer " + '1723187.pt.MdOwsSVBWTaHwOQ5IaD3LZcvBcrsKLQt_l5ewLG4RRP5XNO0PVBIUaoKV1wrQH574yTSyKn48SEz2Mvwfb3ziA',
+    "Harvest-Account-ID": '729138'
+
+  }
+}
 
 console.log(publicPath);
 
@@ -18,7 +31,32 @@ app.use(express.static(publicPath));
 
 app.get('*', function(req, res) {
 	res.sendFile('index.html', {root: publicPath});
-})
+});
+
+
+//Harvest API
+https.get(options, (res) => {
+  const { statusCode } = res;
+
+  if (statusCode !== 200) {
+    console.error(`Request failed with status: ${statusCode}`);
+    return;
+  }
+
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      const parsedData = JSON.parse(rawData);
+      console.log(parsedData);
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+}).on('error', (e) => {
+  console.error(`Got error: ${e.message}`);
+});
 
 //app.use('/mailer', mailRouter);
 
@@ -26,4 +64,4 @@ app.get('*', function(req, res) {
 
 app.listen(port, function() {
 	console.log('Server is running on port ' + port);
-})
+});
