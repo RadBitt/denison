180ac003ce54cdcbb0da8731a758b860b2b65342
Leo <leo@radbitt.com>
2018-11-07 06:04:24 -0800

    base.js

---
+++ src/base.js
@@ -0,0 +1,13 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+	apiKey: "AIzaSyAEeMUbSbK0yJs_6pnerHpa0YvWUQr7IOA",
+    authDomain: "denison-yacht-sales.firebaseapp.com",
+    databaseURL: "https://denison-yacht-sales.firebaseio.com",
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+// named
+export { firebaseApp, base };
